c446b36b9d4bcfc133459f2f82864d5d1a8f06db
NicoCLL <nicocluchsinger@gmail.com>
2022-07-04 17:18:36 -0400

    Bugeado

--- desafio1js/assets/js/index.js
+++ desafio1js/assets/js/index.js
@@ -1,1 +1,18 @@
-element = document.querySelector('#selector')
+
+/*  Guardar cantidades ingresadas en variables*/
+/*  Calcular precio*/
+/*  Traspasar color al circulo*/
+ 
+
+let calculadora = document.getElementById("calculadora");
+let cantidad = document.getElementById("cantidad");
+let precio = document.getElementById("precio");
+let color = document.getElementById("color");
+let boton = document.getElementById("calctotal");
+
+button.addEventListener("click", resultado);
+function resultado(){
+    calculadora.innerHTML = document.getElementById("precio").value * document.getElementById("cantidad").value;
+    cant.innerHTML = document.getElementById("cantidad").value;
+    circ.style.backgroundColor = document.getElementById("color").value;
+}
